8c28a1cf4e7db14819b5eb2b569d8ba7c4d5d828
dingfjaer <42971810+dingfjaer@users.noreply.github.com>
2024-11-07 17:28:00 +0100

    Change back to the original dark og light styles i global.css

--- src/site.config.ts
+++ src/site.config.ts
@@ -72,6 +72,6 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		return `[data-theme="${theme.name}"]`;
 	},
 	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula", "github-light"],
+	themes: ["aurora-x", "github-light"],
 	useThemedScrollbars: false,
 };
